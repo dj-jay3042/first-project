99bc3a3d258bc18b80a8fa4a9f70968aa26b3f69
dj-jay3042 <cjay3042002@gmail.com>
2023-06-14 15:05:53 +0530

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeFile />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeFile from './components/home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeFile
   }
 }
 </script>
---
+++ src/components/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Home Component</h1>
+  Sing : <input type="checkbox" name="hobby" value="Sing" v-model="chk" /><br />
+  Dance :
+  <input type="checkbox" name="hobby" value="Dance" v-model="chk" /><br />
+  <button v-on:click="getData()">Get Values</button>
+
+  <h3>Select Values : {{ chk }}</h3>
+</template>
+<script>
+export default {
+  name: "HomeFile",
+  data() {
+    return {
+      chk: [],
+    };
+  },
+  methods: {
+    getData() {
+      alert("Email: " + this.email + "\nPassword: " + this.passwd);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: crimson;
+}
+</style>
