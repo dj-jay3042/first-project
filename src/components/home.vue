0177faeb27c469160f08e32a4132dd489aec9d2a
dj-jay3042 <cjay3042002@gmail.com>
2023-06-14 15:05:53 +0530

    edit

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Home Component</h1>
+    <button v-on:click="test()">Click to see magic!!!</button>
+</template>
+<script>
+export default {
+    name: "HomeFile"
+}
+</script>
+
+<style scoped>
+h1{
+    color:crimson;
+}
+</style>
